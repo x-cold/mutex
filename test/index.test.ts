cabdebc073d18a3124cb7951413b2f40b6c547b0
x-cold <lxstart@outlook.com>
2023-05-22 21:19:45 +0800

    chore: more test cases

--- test/index.test.ts
+++ test/index.test.ts
@@ -40,12 +40,38 @@ describe('Mutex test', () => {
     expect(result).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
   });
 
-  it('acquire lock', async () => {
+  it('acquire for lock concurrency', async () => {
+    const mutex = new Mutex();
+    const result: number[] = [];
+
+    async function fn(i: number) {
+      await mutex.acquire(async () => {
+        await sleep(100);
+        result.push(i);
+      });
+    }
+
+    const promises = [];
+    for (let i = 0; i < 10; i += 1) {
+      promises.push(fn(i));
+    }
+
+    await Promise.all(promises);
+    expect(result.length).toEqual(10);
+    expect(result).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  });
+
+  it('acquire lock with error', async () => {
     const mutex = new Mutex();
     let cur = 0;
-    await mutex.acquire(async () => {
+    const promise1 = mutex.acquire(async () => {
       cur = 1;
+      throw new Error();
     });
-    expect(cur).toBe(1);
+    const promise2 = mutex.acquire(async () => {
+      cur = 2;
+    });
+    await Promise.all([promise1.catch(() => Promise.resolve()), promise2]);
+    expect(cur).toBe(2);
   });
 });
